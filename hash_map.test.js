6318c2cff2fd7777100d227b366e8e7a4737c969
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-09 04:03:29 +0800

    Initial test when map is empty

--- hash_map.test.js
+++ hash_map.test.js
@@ -1,3 +1,19 @@
 /* hash_map.test.js */
 import { HashMap } from "./hash_map.js";
 
+describe("Hash Map test: 0 items on the list", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Linked List
+    hashmap = new HashMap();
+  });
+  test("Test length() when hash map is empty", () => {
+    expect(hashmap.length()).toEqual(0);
+  });
+  test("Test has() when hash map is empty", () => {
+    expect(hashmap.has("dog")).toEqual(false);
+  });
+  test("Test get() when hash map is empty", () => {
+    expect(hashmap.get("dog")).toEqual(null);
+  });
+});
