40727d5d307c5dd711d7f62119054731ca4a968d
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-09 01:55:32 +0800

    constructor function to set initial value, and empty LinkedList

--- hash_map.js
+++ hash_map.js
@@ -2,6 +2,16 @@
 import { LinkedList, Node } from "./linked_list.js";
 
 class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+    this.keyCount = 0;
+    // prepares a linked list structure for each 'bucket' entry
+    this.list = Array.from({ length: this.capacity }, (_, i) => i).map(
+      () => new LinkedList(),
+    );
+  }
+
 }
 
 export { HashMap };
