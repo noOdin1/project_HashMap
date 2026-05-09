bc0a7e6c4e54cd9cf54f99dd3fa67d76822aee4d
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-10 03:53:11 +0800

    Test on all functions when hash map has 1 entry

--- hash_map.test.js
+++ hash_map.test.js
@@ -57,3 +57,95 @@ describe("Hash Map test: 0 item on the list", () => {
   });
 });
 
+describe("Hash Map test: 1 item on the list", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Linked List
+    hashmap = new HashMap();
+    hashmap.set("apple", "red");
+  });
+
+  test("Test hash() with 1 item on the list", () => {
+    // Hash key must be between 0 and 15
+    expect(hashmap.hash("dummy key")).toBeGreaterThanOrEqual(0);
+    expect(hashmap.hash("dummy key")).toBeLessThan(16);
+  });
+  test("Test testCapacity() with 1 item on the list", () => {
+    expect(hashmap.testCapacity()).toEqual(0.0625);
+  });
+  test("Test set() with 1 item on the list, adding 1 new item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("pineapple", "yellow");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["pineapple", "yellow"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+  });
+  // simulate a collision, 'apple' and 'dog' has the same
+  // hash output '2'
+  test("Test set() with 1 item on the list, adding 1 new item, collision test", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "brown"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+    // value of 0.0625 represent 1 bucket with data
+    expect(hashmap.testCapacity()).toEqual(0.0625);
+  });
+  // test update function for set()
+  test("Test set() with 1 item on the list, update existing key with new value", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("apple", "brown");
+    expect(hashmap.entries()).toEqual([["apple", "brown"]]);
+  });
+  // test update function for set()
+  test("Test set() with 1 item on the list, adding 1 new item, update 2nd item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    hashmap.set("dog", "black");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "black"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+  });
+  test("Test length() when hash map has 1 item", () => {
+    expect(hashmap.length()).toEqual(1);
+  });
+  test("Test has() when hash map has 1 item", () => {
+    expect(hashmap.has("apple")).toEqual(true);
+  });
+  test("Test get() when hash map has 1 item", () => {
+    expect(hashmap.get("apple")).toEqual("red");
+  });
+  test("Test remove() when hash map has 1 item", () => {
+    expect(hashmap.remove("apple")).toEqual(true);
+  });
+  test("Test clear() when hash map has 1 item", () => {
+    hashmap.clear();
+    expect(hashmap.entries()).toEqual([]);
+    expect(hashmap.testCapacity()).toEqual(0);
+  });
+  test("Test search('keys') when hash map has 1 item", () => {
+    expect(hashmap.search("keys")).toEqual(["apple"]);
+  });
+  test("Test search('values') when hash map has 1 item", () => {
+    expect(hashmap.search("values")).toEqual(["red"]);
+  });
+  test("Test search('entries') when hash map has 1 item", () => {
+    expect(hashmap.search("entries")).toEqual([["apple", "red"]]);
+  });
+  test("Test keys() when hash map has 1 item", () => {
+    expect(hashmap.keys()).toEqual(["apple"]);
+  });
+  test("Test values() when hash map has 1 item", () => {
+    expect(hashmap.values()).toEqual(["red"]);
+  });
+  test("Test entries() when hash map has 1 item", () => {
+    expect(hashmap.entries()).toEqual([["apple", "red"]]);
+  });
+
+});
