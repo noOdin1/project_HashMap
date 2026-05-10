2b9d8f3b136e2946328081ceef1f4ead08d38978
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-11 01:03:31 +0800

    Set() section to check current map capacity and expand if necessary

--- hash_map.js
+++ hash_map.js
@@ -68,6 +68,10 @@ class HashMap {
         }
       }
     } else {
+      // check if adding this key will exceed the load capacity
+      if (this.currentCapacity() + 1 / this.capacity > this.loadFactor) {
+        this.expandList();
+      }
       /* This part executes if the key does not exists */
       this.list[hashkey].append(entry);
       this.keyCount++;
