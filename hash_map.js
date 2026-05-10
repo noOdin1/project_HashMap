378a5578743bdbfaa693d5029e1eb8bf30dd7c9f
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-11 01:02:11 +0800

    Making loadFactor and capacity value equal to argumnets passed in

--- hash_map.js
+++ hash_map.js
@@ -3,8 +3,8 @@ import { LinkedList, Node } from "./linked_list.js";
 
 class HashMap {
   constructor(loadFactor = 0.75, capacity = 16) {
-    this.loadFactor = 0.75;
-    this.capacity = 16;
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
     this.keyCount = 0;
     // prepares a linked list structure for each 'bucket' entry
     this.list = Array.from({ length: this.capacity }, (_, i) => i).map(
