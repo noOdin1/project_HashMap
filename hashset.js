6e1cfa5b01f41d97d5b55a7e6ac246577a093ddb
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-13 23:00:58 +0800

    Extra credit section, HashSet, refitting HashMap for HashSet functions

---
+++ hashset.js
@@ -0,0 +1,175 @@
+/* hashset.js */
+import { LinkedList, Node } from "./linked_list.js";
+
+class HashSet {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.keyCount = 0;
+    // prepares a linked list structure for each 'bucket' entry
+    this.list = Array.from({ length: this.capacity }, (_, i) => i).map(
+      () => new LinkedList(),
+    );
+  }
+
+  /* Function to produce a hash for the key supplied */
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 37;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (key.charCodeAt(i) / Math.PI) * primeNumber + hashCode;
+    }
+    // hashCode = Math.floor(hashCode / primeNumber);
+    hashCode = Math.floor((hashCode %= this.capacity));
+
+    return hashCode;
+  }
+
+  // This is part of the objective outline on the Project Page.
+  // Expand the list when the load capacity is at 75%.
+  expandList() {
+    let tmpStore = this.search("entries");
+    // expand capacity first (by doubling it), load factor remains the same
+    this.capacity *= 2;
+    this.list = Array.from({ length: this.capacity }, (_, i) => i).map(
+      () => new LinkedList(),
+    );
+    this.keyCount = 0; // reset the keyCount;
+    tmpStore.forEach((x) => this.set(x[0], x[1]));
+  }
+
+  /**
+   * The definition for capacity here is the amount of 'bucket' with
+   * at least 1 item over overall number of 'bucket'.
+   */
+  currentCapacity() {
+    let count = 0;
+    this.list.forEach((x) => (x.size() == 0 ? 0 : count++));
+    return count / this.capacity;
+  }
+
+  /**
+   * Function to store the [key, value] pair on the hash map
+   * Although not specified, this function will call hash()
+   * function to create a hash entry.
+   **/
+  set(key) {
+    let hashkey = this.hash(key);
+    let entry = [key];
+
+    /* this would only trigger if the key exists */
+    if (this.has(key)) {
+      // update the key
+      let tmpLinkedList = this.list[hashkey];
+      for (let i = 1; i <= tmpLinkedList.size(); i++) {
+        if (tmpLinkedList.valueAt(i)[0] === key) {
+          tmpLinkedList.changeValueAt(i, entry);
+        }
+      }
+    } else {
+      // check if adding this key will exceed the load capacity
+      if (this.currentCapacity() + 1 / this.capacity > this.loadFactor) {
+        this.expandList();
+      }
+
+      /* This part executes if the key does not exists */
+      this.list[hashkey].append(entry);
+      this.keyCount++;
+    }
+  }
+
+  /* Function to get the value corresponding to the key */
+  get(key) {
+    let hashkey = this.hash(key);
+    if (!(this.list[hashkey].size() > 0)) {
+      return null;
+    }
+    let tmpLinkedList = this.list[hashkey];
+    for (let i = 1; i <= tmpLinkedList.size(); i++) {
+      if (tmpLinkedList.valueAt(i)[0] === key) {
+        return tmpLinkedList.valueAt(i)[1];
+      }
+    }
+    // This statement is used when a 'key' is hashed
+    // and points to a bucket, but the 'key' is not matched
+    // with any of the items in the bucket.
+    return null;
+  }
+
+  /* Function to check if the hash map contains the key */
+  has(key) {
+    return this.get(key) === null ? false : true;
+  }
+
+  /**
+   * Function to remove the [key,value] pair on the hash map.
+   * Returns true if key is found and removed, false otherwise.
+   **/
+  remove(key) {
+    let hashkey = this.hash(key);
+    if (!(this.list[hashkey].size() > 0)) {
+      return false;
+    }
+    let tmpLinkedList = this.list[hashkey];
+    for (let i = 1; i <= tmpLinkedList.size(); i++) {
+      if (tmpLinkedList.valueAt(i)[0] === key) {
+        tmpLinkedList.removeAt(i);
+        return true;
+      }
+    }
+    // This statement is used when a 'key' is hashed
+    // and points to a bucket, but the 'key' is not matched
+    // with any of the items in the bucket.
+    return false;
+  }
+  /**
+   * Function to return the number of keys stored in the
+   *  hash map
+   **/
+  length() {
+    return this.keyCount;
+  }
+
+  /* Function to remove all entries on the hash */
+  clear() {
+    this.list = Array.from({ length: this.capacity }, (_, i) => i).map(
+      () => new LinkedList(),
+    );
+  }
+
+  /* Function to search the list */
+  search(criteria) {
+    let tmpArr = [];
+    /* This loop contains another loop which would not scale properly */
+    this.list.forEach((x) => {
+      if (x.size() != 0) {
+        for (let i = 1; i <= x.size(); i++) {
+          criteria === "keys"
+            ? tmpArr.push(x.valueAt(i)[0])
+            : criteria === "values"
+              ? tmpArr.push(x.valueAt(i)[1])
+              : tmpArr.push(x.valueAt(i));
+        }
+      }
+    });
+    return tmpArr;
+  }
+
+  /* Function to return an arrray of all keys on the hash */
+  keys() {
+    return this.search("keys");
+  }
+
+  /* Function to return an arrray of all values from the key value pair */
+  // values() {
+  //   return this.search("values");
+  // }
+
+  /* Function to return an arrray of all key, value pair on the hash map */
+  entries() {
+    return this.search("entries");
+  }
+}
+
+export { HashSet };
---
+++ hashset.test.js
@@ -0,0 +1,307 @@
+/* hashset.test.js */
+import { HashSet } from "./hashset.js";
+
+describe("Hash Map test: 0 item on the list", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Hash Map
+    hashmap = new HashMap();
+  });
+
+  test("Test hash() with empty list", () => {
+    // Hash key must be between 0 and 15
+    expect(hashmap.hash("dummy key")).toBeGreaterThanOrEqual(0);
+    expect(hashmap.hash("dummy key")).toBeLessThan(16);
+  });
+  test("Test currentCapacity() with empty list", () => {
+    expect(hashmap.currentCapacity()).toEqual(0);
+  });
+  test("Test set() with empty list", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("pineapple", "yellow");
+    expect(hashmap.entries()).toEqual([["pineapple", "yellow"]]);
+  });
+  test("Test length() when hash map is empty", () => {
+    expect(hashmap.length()).toEqual(0);
+  });
+  test("Test has() when hash map is empty", () => {
+    expect(hashmap.has("dog")).toEqual(false);
+  });
+  test("Test get() when hash map is empty", () => {
+    expect(hashmap.get("dog")).toEqual(null);
+  });
+  test("Test remove() when hash map is empty", () => {
+    expect(hashmap.remove("dog")).toEqual(false);
+  });
+  test("Test clear() when hash map is empty", () => {
+    hashmap.clear();
+    expect(hashmap.entries()).toEqual([]);
+  });
+  test("Test search('keys') when hash map is empty", () => {
+    expect(hashmap.search("keys")).toEqual([]);
+  });
+  test("Test search('values') when hash map is empty", () => {
+    expect(hashmap.search("values")).toEqual([]);
+  });
+  test("Test search('entries') when hash map is empty", () => {
+    expect(hashmap.search("entries")).toEqual([]);
+  });
+  test("Test keys() when hash map is empty", () => {
+    expect(hashmap.keys()).toEqual([]);
+  });
+  test("Test values() when hash map is empty", () => {
+    expect(hashmap.values()).toEqual([]);
+  });
+  test("Test entries() when hash map is empty", () => {
+    expect(hashmap.entries()).toEqual([]);
+  });
+});
+
+describe("Hash Map test: 1 item on the list", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Hash Map
+    hashmap = new HashMap();
+    hashmap.set("apple", "red");
+  });
+
+  test("Test hash() with 1 item on the list", () => {
+    // Hash key must be between 0 and 15
+    expect(hashmap.hash("dummy key")).toBeGreaterThanOrEqual(0);
+    expect(hashmap.hash("dummy key")).toBeLessThan(16);
+  });
+  test("Test currentCapacity() with 1 item on the list", () => {
+    expect(hashmap.currentCapacity()).toEqual(0.0625);
+  });
+  test("Test set() with 1 item on the list, adding 1 new item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("pineapple", "yellow");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["pineapple", "yellow"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+  });
+  // simulate a collision, 'apple' and 'dog' has the same
+  // hash output '2'
+  test("Test set() with 1 item on the list, adding 1 new item, collision test", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "brown"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+    // value of 0.0625 represent 1 bucket with data
+    expect(hashmap.currentCapacity()).toEqual(0.0625);
+  });
+  // test update function for set()
+  test("Test set() with 1 item on the list, update existing key with new value", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("apple", "brown");
+    expect(hashmap.entries()).toEqual([["apple", "brown"]]);
+  });
+  // test update function for set()
+  test("Test set() with 1 item on the list, adding 1 new item, update 2nd/new item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    hashmap.set("dog", "black");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "black"],
+    ]);
+    expect(hashmap.length()).toEqual(2);
+  });
+  test("Test length() when hash map has 1 item", () => {
+    expect(hashmap.length()).toEqual(1);
+  });
+  test("Test has() when hash map has 1 item", () => {
+    expect(hashmap.has("apple")).toEqual(true);
+  });
+  test("Test get() when hash map has 1 item", () => {
+    expect(hashmap.get("apple")).toEqual("red");
+  });
+  test("Test remove() when hash map has 1 item", () => {
+    expect(hashmap.remove("apple")).toEqual(true);
+  });
+  test("Test clear() when hash map has 1 item", () => {
+    hashmap.clear();
+    expect(hashmap.entries()).toEqual([]);
+    expect(hashmap.currentCapacity()).toEqual(0);
+  });
+  test("Test search('keys') when hash map has 1 item", () => {
+    expect(hashmap.search("keys")).toEqual(["apple"]);
+  });
+  test("Test search('values') when hash map has 1 item", () => {
+    expect(hashmap.search("values")).toEqual(["red"]);
+  });
+  test("Test search('entries') when hash map has 1 item", () => {
+    expect(hashmap.search("entries")).toEqual([["apple", "red"]]);
+  });
+  test("Test keys() when hash map has 1 item", () => {
+    expect(hashmap.keys()).toEqual(["apple"]);
+  });
+  test("Test values() when hash map has 1 item", () => {
+    expect(hashmap.values()).toEqual(["red"]);
+  });
+  test("Test entries() when hash map has 1 item", () => {
+    expect(hashmap.entries()).toEqual([["apple", "red"]]);
+  });
+});
+
+describe("Hash Map test: 2 item on the list", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Hash Map
+    hashmap = new HashMap();
+    hashmap.set("apple", "red");
+    hashmap.set("banana", "yellow");
+  });
+
+  test("Test hash() with 2 item on the list", () => {
+    // Hash key must be between 0 and 15
+    expect(hashmap.hash("dummy key")).toBeGreaterThanOrEqual(0);
+    expect(hashmap.hash("dummy key")).toBeLessThan(16);
+  });
+  test("Test expandList() with 2 item on the list", () => {
+    // Hash key must be between 0 and 15
+    expect(hashmap.length()).toEqual(2);
+    expect(hashmap.currentCapacity()).toEqual(0.125);
+    hashmap.expandList();
+    expect(hashmap.length()).toEqual(2);
+    expect(hashmap.currentCapacity()).toEqual(0.0625);
+  });
+  test("Test currentCapacity() with 2 item on the list", () => {
+    expect(hashmap.currentCapacity()).toEqual(0.125);
+  });
+  test("Test set() with 2 item on the list, adding 1 new item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("pineapple", "yellow");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["pineapple", "yellow"],
+      ["banana", "yellow"],
+    ]);
+    expect(hashmap.length()).toEqual(3);
+  });
+  // simulate a collision, 'apple' and 'dog' has the same
+  // hash output '2'
+  test("Test set() with 2 item on the list, adding 1 new item, collision test", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "brown"],
+      ["banana", "yellow"],
+    ]);
+    expect(hashmap.length()).toEqual(3);
+    // value of 0.0625 represent 1 bucket with data
+    // This value is a measurement of buckets with data / overall buckets
+    // Because 'dog' is a collision with 'apple', it will be stored in the same
+    // bucket, therefore 'buckets' with data is still 2
+    expect(hashmap.currentCapacity()).toEqual(0.125);
+  });
+  // test update function for set()
+  test("Test set() with 2 item on the list, update existing key with new value", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("apple", "brown");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "brown"],
+      ["banana", "yellow"],
+    ]);
+  });
+  // test update function for set()
+  test("Test set() with 2 item on the list, adding 1 new item, update 2nd/new item", () => {
+    // After value insertion check if it's present on the list
+    hashmap.set("dog", "brown");
+    hashmap.set("dog", "black");
+    expect(hashmap.entries()).toEqual([
+      ["apple", "red"],
+      ["dog", "black"],
+      ["banana", "yellow"],
+    ]);
+    expect(hashmap.length()).toEqual(3);
+    expect(hashmap.currentCapacity()).toEqual(0.125);
+  });
+  test("Test length() when hash map has 2 items", () => {
+    expect(hashmap.length()).toEqual(2);
+  });
+  test("Test has() when hash map has 2 items", () => {
+    expect(hashmap.has("apple")).toEqual(true);
+  });
+  test("Test get() when hash map has 2 items", () => {
+    expect(hashmap.get("apple")).toEqual("red");
+  });
+  test("Test remove() when hash map has 2 items", () => {
+    expect(hashmap.remove("apple")).toEqual(true);
+  });
+  test("Test clear() when hash map has 2 items", () => {
+    expect(hashmap.currentCapacity()).toEqual(0.125);
+    expect(hashmap.length()).toEqual(2);
+    hashmap.clear();
+    expect(hashmap.entries()).toEqual([]);
+    expect(hashmap.currentCapacity()).toEqual(0);
+  });
+  test("Test search('keys') when hash map has 2 items", () => {
+    expect(hashmap.search("keys")).toEqual(["apple", "banana"]);
+  });
+  test("Test search('values') when hash map has 2 items", () => {
+    expect(hashmap.search("values")).toEqual(["red", "yellow"]);
+  });
+  test("Test search('entries') when hash map has 2 items", () => {
+    expect(hashmap.search("entries")).toEqual([
+      ["apple", "red"],
+      ["banana", "yellow"],
+    ]);
+  });
+  test("Test keys() when hash map has 2 items", () => {
+    expect(hashmap.search("keys")).toEqual(["apple", "banana"]);
+  });
+  test("Test values() when hash map has 2 items", () => {
+    expect(hashmap.search("values")).toEqual(["red", "yellow"]);
+  });
+  test("Test entries() when hash map has 2 items", () => {
+    expect(hashmap.search("entries")).toEqual([
+      ["apple", "red"],
+      ["banana", "yellow"],
+    ]);
+  });
+});
+
+describe("Test specifically to determine if Hash Map can expand dynamically", () => {
+  let hashmap;
+  beforeEach(() => {
+    // before each test, reset tmpList to a new empty Hash Map
+    hashmap = new HashMap();
+    hashmap.set("apple", "red");
+    hashmap.set("banana", "yellow");
+  });
+
+  test("Test to see if Hash Map will expand when load capacity exceeds 0.75", () => {
+    // hashmap.set("appple", "red");
+    // hashmap.set("banana", "yellow");
+    hashmap.set("carrot", "orange");
+    hashmap.set("dog", "brown");
+    hashmap.set("elephant", "gray");
+    hashmap.set("frog", "green");
+    hashmap.set("grape", "purple");
+    hashmap.set("hat", "black");
+    hashmap.set("ice cream", "white");
+    hashmap.set("jacket", "blue");
+    hashmap.set("kite", "pink");
+    hashmap.set("lion", "golden");
+    hashmap.set("phone", "black");
+    hashmap.set("coffee", "white");
+    // After adding the above key, the list is now at capacity 75%.
+    // console.log("The current capacity is: " + hashmap.currentCapacity());
+    expect(hashmap.length()).toEqual(14);
+    expect(hashmap.currentCapacity()).toEqual(0.75);
+    hashmap.set("plane", "white");
+    // console.log("The current capacity is: " + hashmap.currentCapacity());
+    expect(hashmap.length()).toEqual(15);
+    expect(hashmap.currentCapacity()).toEqual(15 / 32);
+    hashmap.set("moon", "silver");
+    expect(hashmap.length()).toEqual(16);
+    expect(hashmap.currentCapacity()).toEqual(16 / 32);
+  });
+});
